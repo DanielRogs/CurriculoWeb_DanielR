4066ccc20a4722c10a7f01f9fd3461ebf75888b6
DanielRogs <danielrocha.dr142@gmail.com>
2025-04-06 21:24:33 -0300

    feat(header): adicionando responsividade

--- apps/web/src/components/Header/index.jsx
+++ apps/web/src/components/Header/index.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 import { Container, Lista } from "./styled";
+import { IoMenu } from "react-icons/io5";
 
-const Header = () => {
+const Header = ({ language = "ptbr" }) => {
+  const handleLanguageChange = (lang) => {
+    language = lang;
+  };
+
   return (
     <Container>
       <Lista>
@@ -11,6 +16,32 @@ const Header = () => {
         <li>Projetos</li>
         <li>Contato</li>
       </Lista>
+      <div className="language-selection">
+        <span
+          style={
+            language === "ptbr"
+              ? { color: "var(--secondary-color)" }
+              : { color: "var(--tertiary-color)" }
+          }
+          onClick={() => handleLanguageChange("ptbr")}
+        >
+          PT
+        </span>
+        <span
+          style={
+            language === "en"
+              ? { color: "var(--secondary-color)" }
+              : { color: "var(--tertiary-color)" }
+          }
+          onClick={() => handleLanguageChange("en")}
+        >
+          EN
+        </span>
+      </div>
+
+      <div className="mobile-menu">
+        <IoMenu className="icon" />
+      </div>
     </Container>
   );
 };
--- apps/web/src/components/Header/styled.js
+++ apps/web/src/components/Header/styled.js
@@ -12,6 +12,52 @@ export const Container = styled.nav`
   padding-top: 10px;
   z-index: 100;
   background: var(--primary-color);
+
+  .language-selection {
+    display: flex;
+    gap: 10px;
+    margin-left: auto;
+    margin-right: 50px;
+    margin-block: 19px;
+
+    span {
+      color: white;
+      font-family: var(--font-family);
+      font-weight: bold;
+      cursor: pointer;
+      transition: 200ms;
+
+      &:hover {
+        color: var(--secondary-color);
+        text-decoration: underline;
+      }
+    }
+  }
+
+  .mobile-menu {
+    display: none;
+
+    .icon {
+      font-size: 25px;
+      margin-right: 20px;
+      color: white;
+    }
+  }
+
+  @media (max-width: 785px) {
+    width: 100%;
+    justify-content: space-between;
+
+    .mobile-menu {
+      display: flex;
+    }
+
+    .language-selection{
+      margin-right: 0px;
+      margin-left: 20px;
+
+    }
+  }
 `;
 
 export const Lista = styled.ul`
@@ -32,5 +78,9 @@ export const Lista = styled.ul`
       color: var(--secondary-color);
       text-decoration: underline;
     }
+  }
+
+  @media (max-width: 785px) {
+    display: none;
   }
 `;
